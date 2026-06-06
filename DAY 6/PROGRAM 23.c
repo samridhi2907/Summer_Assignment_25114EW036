e0fc7e59ba28d18ff0e787847ddd6ace42b2c846
samridhi2907 <25114ew036@glbitm.ac.in>
2026-06-06 22:12:46 +0530

    Create PROGRAM 23.c

---
+++ DAY 6/PROGRAM 23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ int main()
+{
+   int number;
+    int count = 0;
+ 
+    printf("Enter the unsigned integer:\n");
+    scanf("%d", &number);
+    while (number != 0)
+    {
+        if ((number & 1) == 1)
+            count++;
+        number = number >> 1;
+    }
+    printf("number of one's are :\n%d\n", count);
+    return 0;
+}
