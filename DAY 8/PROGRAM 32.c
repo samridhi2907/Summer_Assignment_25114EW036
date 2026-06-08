83a1a3f54504ea23d090747abfa6d33ce4a27e86
samridhi2907 <25114ew036@glbitm.ac.in>
2026-06-08 18:49:55 +0530

    Create PROGRAM 32.c

---
+++ DAY 8/PROGRAM 32.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+void main()
+{
+    int i, j, rows=5; 
+
+    for (i = 1; i <= rows; i++) 
+    {  
+        for (j = 1; j <= i; j++)  
+            printf("%d", i);  
+
+        printf("\n");  
+    }
+}
