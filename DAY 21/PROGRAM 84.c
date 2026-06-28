51afa70e771869eafbd2636f7ef9f8bf297e8763
samridhi2907 <25114ew036@glbitm.ac.in>
2026-06-28 10:29:51 +0530

    Create PROGRAM 84.c

---
+++ DAY 21/PROGRAM 84.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    ch = 'g';
+    printf("%c in uppercase is represented as  %c",
+           ch, toupper(ch));
+
+    return 0;
+}
